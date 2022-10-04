07fc9685502a0907272f135e40934060244f8eaa
Thiago Castilho <tdecasti@my.centennialcollege.ca>
2022-10-03 21:50:28 -0400

    Passport Configuration Completed

---
+++ app/routes/auth.route.server.js
@@ -0,0 +1,19 @@
+import {Router} from 'express';
+
+const router = Router();
+
+// Display Login Page
+router.get('/login', DisplayLoginPage);
+// Process Login Page
+router.post('/login', ProcessLoginPage);
+
+
+// Display Registration Page
+router.get('/register', DisplayRegisterPage);
+// Process Registration page
+router.post('/regiser', ProcessRegisterPage);
+
+// Process Logout Page
+router.get('/logout', ProcessLogoutPage);
+
+export default router;
